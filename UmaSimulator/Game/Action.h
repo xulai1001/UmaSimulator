889e13776d225b38275864cde0b6385afb94c7da
h zy <2658628026@qq.com>
2025-03-02 19:13:56 +0800

    定义game类

--- UmaSimulator/Game/Action.h
+++ UmaSimulator/Game/Action.h
@@ -16,30 +16,8 @@ enum TrainActionTypeEnum :int16_t
   TRA_none = -1, //此Action不训练，只做菜
   TRA_redistributeCardsForTest = -2 //使用这个标记时，说明要randomDistributeCards，用于测试ai分数，在Search::searchSingleActionThread中使用
 };
-enum DishTypeEnum :int16_t
-{
-  DISH_none = 0,
-  DISH_sandwich, //speed+power+wiz 25%
-  DISH_curry, //speed+stamina+guts 25%
-  DISH_speed1,//150+80  60%
-  DISH_stamina1,
-  DISH_power1,
-  DISH_guts1,
-  DISH_wiz1,
-  DISH_speed2,//250+80  90%~100%
-  DISH_stamina2,
-  DISH_power2,
-  DISH_guts2,
-  DISH_wiz2,
-  DISH_g1plate //5*80
-};
-//考虑到这个剧本做菜有随机性，可能会影响希望选的训练
-//一个回合可以拆成两步：先做菜，再训练，也可以不拆
-//Action类表示做菜，或者训练，或者做菜+训练
-//“标准的Action”是做菜或者训练，不包括 做菜+训练 和 不做菜也不训练
-//标准的Action有编号，8+13=21种
-//搜索时，每次只搜一层“标准的Action”
-//ura比赛时，两个合法的action是做菜+不选训练（自动比赛）和不做菜+选择比赛
+
+
 struct Action 
 {
   static const std::string trainingName[8];
@@ -47,6 +25,7 @@ struct Action
   static const Action Action_RedistributeCardsForTest;
   static const int MAX_ACTION_TYPE = 21;//标准的Action有编号，8+13=21种
   static const int MAX_TWOSTAGE_ACTION_TYPE = 21 + 8 + 8;//二阶段搜索考虑的最多Action个数，只有两种1级菜需要考虑二阶段搜索，8+13+2*8=37种
+
 
   
   int16_t dishType;//做菜，0为不做菜
--- UmaSimulator/Game/Game.h
+++ UmaSimulator/Game/Game.h
@@ -8,12 +8,71 @@
 struct SearchParam;
 
 
-enum FarmUpgradeStrategyEnum :int16_t
+enum LegendColorEnum :int16_t
 {
-  FUS_default,//默认（调试出来的最优）
-  FUS_noGarlicLv3,//不升级Lv3大蒜
-  FUS_garlicLv3,//升级Lv3大蒜
-  FUS_none,//不自动升级
+  L_blue,//蓝
+  L_green,//绿
+  L_red,//红（粉）
+  L_unknown=-1,
+};
+
+//剧本“心得”
+//游戏里id是 4位数"XY0Z"里X是颜色，Y是星数，Z是第几个
+//此处-1是空，57个buff用0~56表示
+struct ScenarioBuffInfo
+{
+  int16_t buffId;
+  bool isActive;
+  int16_t coolTime;
+  ScenarioBuffInfo();
+  int16_t getBuffColor() const;
+  int16_t getBuffStar() const;
+  std::string getName() const;
+};
+
+//剧本加成   //，只考虑五个训练共有的部分，按人头的不算
+//每次计算训练数值前都会重新计算这个
+struct ScenarioBonus
+{
+  float hintProb;//红点概率提升%
+  int16_t hintLv;//红点等级提升
+  bool doubleHint;//追加一个hint
+  
+  float vitalReduce;//体力消耗减少%
+  float jibanAdd;//羁绊增加量
+  float deyilv;//得意率提升%
+  float disappearRateReduce;//消失率减少。原本的普通卡消失率是50，友人100。有对应buff时减少25
+
+  float xunlian;//训练加成%
+  float ganjing;//干劲加成%
+  float youqing;//友情加成%
+
+  int16_t extraHead;//追加人头的个数
+
+  float xunlianPerHead;//每个人头额外多少训练加成
+  float ganjingPerHead;//每个人头额外多少干劲加成
+  float youqingPerShiningHead;//每个闪彩人头额外多少友情加成
+  ScenarioBonus();
+  void clear();
+};
+
+
+struct GameSettings
+{
+  //显示相关
+  bool playerPrint;//给人玩的时候，显示更多信息
+
+  //参数设置
+
+  float ptScoreRate;//每pt多少分
+  float hintPtRate;//每一级hint等价多少pt
+  float hintProbTimeConstant;//已经有t级技能hint时，令hint出技能的概率=0.9*(1-exp(-exp(2-t/T)))，其中T=hintProbTimeConstant，默认80
+  int16_t eventStrength;//每回合有（待测）概率加这么多属性，模拟支援卡事件
+  int16_t scoringMode;//评分方式
+
+  int16_t color_priority;//优先选择哪种颜色，-1为无优先
+
+  GameSettings();
 };
 
 enum scoringModeEnum :int16_t
@@ -31,29 +90,86 @@ enum scoringModeEnum :int16_t
 
 enum personIdEnum :int16_t
 {
-  PSID_none = -1,//未分配
-  PSID_noncardYayoi = 6,//非卡理事长
-  PSID_noncardReporter = 7,//非卡记者
-  PSID_npc = 8//NPC
+  PS_none = -1,//未分配
+  PS_noncardYayoi = 6,//非卡理事长
+  PS_noncardReporter = 7,//非卡记者
+  PS_npc = 8,//NPC（备用）
+  PS_npc0 = 10,//NPC速
+  PS_npc1 = 11,//NPC耐
+  PS_npc2 = 12,//NPC力
+  PS_npc3 = 13,//NPC根
+  PS_npc4 = 14,//NPC智
 };
 
-enum gameStageEnum :int16_t
+enum LegendColorEnum :int16_t
 {
-  GameStage_beforeTrain = 0,//训练（或比赛）前
-  GameStage_afterTrain = 1,//训练后，处理事件前
+  L_blue,//蓝
+  L_green,//绿
+  L_red,//红（粉）
+  L_unknown = -1,
 };
+
+//int16_t stage;//分配人头前1，分配人头后c2，训练后有团卡事件（选出行或三选一）3，抽取心得前4（如果有），选心得5（如果有），固定与随机事件前6，6之后进入stage1
+//对于神经网络stage2可以计算policy，stage4/6可以计算value
+//stage2时神经网络直接输出选择的训练
+//stage3/5时，列举所有选项分别进行一步（此过程没有随机性），然后分别调用神经网络计算value，取value最高的作为选项
+enum StageEnum :int16_t
+{
+  ST_none,
+  ST_distribute,//分配人头前
+  ST_train,//训练前
+  ST_decideEvent,//选择团卡事件前
+  ST_pickBuff,//随机抽取心得前
+  ST_chooseBuff,//选心得前
+  ST_event,//处理事件前
+};
+
+//int16_t decidingEvent;//需要处理的含选择项的事件。选buff 1，团卡出行 2，团卡三选一 3
+enum DecidingEventEnum :int16_t
+{
+  DecidingEvent_none,
+  DecidingEvent_chooseBuff,//选buff
+  DecidingEvent_outing,//团卡出行
+  DecidingEvent_three,//团卡三选一
+};
+
+enum TrainActionTypeEnum :int16_t
+{
+  T_speed = 0,
+  T_stamina,
+  T_power,
+  T_guts,
+  T_wiz,
+  T_rest,
+  T_outgoing, //包括合宿的“休息&外出”
+  T_race, //包括生涯比赛
+  T_none = -1, //此Action不训练，只做菜
+  //TRA_redistributeCardsForTest = -2 //使用这个标记时，说明要randomDistributeCards，用于测试ai分数，在Search::searchSingleActionThread中使用
+};
+
+
+struct Action
+{
+  static const std::string trainingName[8];
+  //static const Action Action_RedistributeCardsForTest;
+  //static const int MAX_ACTION_TYPE = 21;//标准的Action有编号，8+13=21种
+  //static const int MAX_TWOSTAGE_ACTION_TYPE = 21 + 8 + 8;//二阶段搜索考虑的最多Action个数，只有两种1级菜需要考虑二阶段搜索，8+13+2*8=37种
+
+  int16_t stage;//这个action是作用于哪个stage的，如果是ST_distribute、ST_pickBuff、ST_event这三个不需要做出任何选择的stage，则无视以下内容
+  int16_t idx;//stage=ST_train时为训练，01234速耐力根智，5外出，6休息，7比赛。stage=ST_decideEvent和ST_chooseBuff时是选第几个
+  Action();//空Action
+  Action(int st);//ST_distribute、ST_pickBuff、ST_event这三个不需要做出任何选择的stage
+  Action(int st, int idx);//需要做选择的stage
+
+  //bool isActionStandard() const;
+  //int toInt() const;
+  std::string toString() const;
+  //static Action intToAction(int i);
+};
+
 struct Game
 {
-  //显示相关
-  bool playerPrint;//给人玩的时候，显示更多信息
-
-  //参数设置
-
-  float ptScoreRate;//每pt多少分
-  float hintPtRate;//每一级hint等价多少pt
-  int16_t eventStrength;//每回合有（待测）概率加这么多属性，模拟支援卡事件
-  int16_t farmUpgradeStrategy;//升级农田的策略
-  int16_t scoringMode;//评分方式
+  GameSettings gameSettings;//用户设置
 
   //基本状态，不包括当前回合的训练信息
   int32_t umaId;//马娘编号，见KnownUmas.cpp
@@ -65,12 +181,13 @@ struct Game
   int16_t gameStage;//游戏阶段，0是训练前，1是训练后
   int16_t vital;//体力，叫做“vital”是因为游戏里就这样叫的
   int16_t maxVital;//体力上限
-  int16_t motivation;//干劲，从1到5分别是绝不调到绝好调
+  int16_t motivation;//干劲，从1到5分别是绝不调到绝好调，超绝好调不在这里
 
   int16_t fiveStatus[5];//五维属性，1200以上不减半
   int16_t fiveStatusLimit[5];//五维属性上限，1200以上不减半
-  int16_t skillPt;//技能点
-  int16_t skillScore;//已买技能的分数
+  int32_t skillPt;//技能点
+  int32_t skillScore;//已买技能的分数
+  int32_t hintSkillLvCount;//已经有多少级hint的技能了。hintSkillLvCount越多，hint出技能的概率越小，出属性的概率越大。
   int16_t trainLevelCount[5];//训练等级计数，每点4下加一级
 
   int16_t failureRateBias;//失败率改变量。练习上手=-2，练习下手=2
@@ -81,52 +198,57 @@ struct Game
 
   int16_t zhongMaBlueCount[5];//种马的蓝因子个数，假设只有3星
   int16_t zhongMaExtraBonus[6];//种马的剧本因子以及技能白因子（等效成pt），每次继承加多少。全大师杯因子典型值大约是30速30力200pt
-  
+
+  int16_t stage;//int16_t stage;//分配人头前1，分配人头后c2，训练后有团卡事件（选出行或三选一）3，抽取心得前4（如果有），选心得5（如果有），固定与随机事件前6，6之后进入stage1
+  int16_t decidingEvent;//需要处理的含选择项的事件。选buff 1，团卡出行 2，团卡三选一 3
   bool isRacing;//这个回合是否在比赛
 
   int16_t friendship_noncard_yayoi;//非卡理事长羁绊
   int16_t friendship_noncard_reporter;//非卡记者羁绊
 
   Person persons[MAX_INFO_PERSON_NUM];//依次是6张卡。非卡理事长，记者，NPC们不单独分配person类，编号一律8
-  int16_t personDistribution[5][5];//每个训练有哪些人头id，personDistribution[哪个训练][第几个人头]，空位置为-1，0~5是6张卡，非卡理事长6，记者7，NPC们编号一律8
+  int16_t personDistribution[5][5];//每个训练有哪些人头id，personDistribution[哪个训练][第几个人头]，空位置为-1，0~5是6张卡，非卡理事长6，记者7，NPC参见personIdEnum
   //int lockedTrainingId;//是否锁训练，以及锁在了哪个训练。可以先不加，等ai做完了有时间再加。
 
   int16_t saihou;//赛后加成
 
-  std::discrete_distribution<> distribution_noncard;//非卡理事长/记者的分布
-  std::discrete_distribution<> distribution_npc;//npc的分布
 
   //剧本相关--------------------------------------------------------------------------------------
   
-  //状态相关
-  int16_t cook_material[5];//五种菜个数
-  int32_t cook_dish_pt;//料理pt
-  int32_t cook_dish_pt_turn_begin;//回合刚开始（吃菜之前）的料理pt：用来检查料理pt相关的升级
-  int16_t cook_farm_level[5];//五种农田的等级
-  int16_t cook_farm_pt;//农田升级pt
-  bool cook_dish_sure_success;//大成功确定
-  int16_t cook_dish;//当前生效的菜
-  int16_t cook_win_history[5];//五次试食会是否“大满足”，非大满足0，大满足1，超满足2
-
-  //最终收获值=f(cook_harvest_green_count)*(基本收获 + cook_harvest_history*追加收获 + cook_harvest_extra)
-  int16_t cook_harvest_history[4];//此4回合分别是哪4种菜，-1是还未选择
-  bool cook_harvest_green_history[4];//此4回合是不是绿菜
-  int16_t cook_harvest_extra[5];//每回合收获=追加收获+人头数。cook_harvest_extra是人头数累计菜量
-  
+  int16_t lg_mainColor;//主色
+  int16_t lg_gauge[3];//三种颜色的格数
+  int16_t lg_trainingColor[8];//训练的gauge颜色
+  bool lg_trainingColorBoost[8];//训练的gauge是否+3
 
 
-  //菜量获取相关
-  int16_t cook_train_material_type[8];//训练外出比赛获得的菜的种类，编号参考TrainActionTypeEnum
-  bool cook_train_green[8];//训练外出比赛是否为绿圈
-  int16_t cook_main_race_material_type;//比赛回合的菜的种类
+  ScenarioBuffInfo lg_buffs[10];//10个buff，空则buffId=0
+  bool lg_haveBuff[57];//有哪些buff，和lg_buffs重复但是便于查找
+  int16_t lg_pickedBuffsNum;//抽取到了几个buff
+  int16_t lg_pickedBuffs[9];//抽取到的buff的id
+
+
+  int16_t lg_blue_active;//蓝登的超绝好调
+  int16_t lg_blue_remainCount;//超绝好调还剩几个回合
+  int16_t lg_blue_currentStepCount;//满3格启动超绝好调
+  int16_t lg_blue_canExtendCount;//还能延长几次
+
+  int16_t lg_green_todo;//绿登
+
+  int16_t lg_red_friendsGauge[16];//红登的羁绊条，编号和personIdEnum对应
+  int16_t lg_red_friendsLv[16];//红登的等级条，编号和personIdEnum对应
+
+
 
   //单独处理剧本友人卡，因为接近必带。其他友人团队卡的以后再考虑
   int16_t friend_type;//0没带友人卡，1 ssr卡，2 r卡
   int16_t friend_personId;//友人卡在persons里的编号
-  int16_t friend_stage;//0是未点击，1是已点击但未解锁出行，2是已解锁出行
-  int16_t friend_outgoingUsed;//友人的出行已经走了几段了   暂时不考虑其他友人团队卡的出行
   double friend_vitalBonus;//友人卡的回复量倍数
   double friend_statusBonus;//友人卡的事件效果倍数
+
+  int16_t friend_stage;//0是未点击，1是已点击但未解锁出行，2是已解锁出行
+  bool friend_outgoingUsed[5];//友人的出行哪几段走过了   暂时不考虑其他友人团队卡的出行
+  bool friend_qingre;//团卡是否情热
+  int16_t friend_qingreTurn;//团卡连续情热多少回合了
 
 
 
@@ -136,13 +258,7 @@ struct Game
   int16_t trainValue[5][6];//训练数值的总数（下层+上层），第一个数是第几个训练，第二个数依次是速耐力根智pt
   int16_t trainVitalChange[5];//训练后的体力变化（负的体力消耗）
   int16_t failRate[5];//训练失败率
-  bool isTrainShining[5];//训练是否闪彩
-
-  int16_t cook_dishpt_success_rate;//大成功率
-  int16_t cook_dishpt_training_bonus;//料理pt训练加成
-  int16_t cook_dishpt_skillpt_bonus;//料理pt技能点加成
-  int16_t cook_dishpt_deyilv_bonus;//料理pt得意率加成
-  int16_t cook_train_material_num_extra[8];//训练外出比赛获得的菜的个数增加（cook_harvest_extra），训练=非link卡人头数+3*link卡数，外出比赛=0
+  int16_t trainShiningNum[5];//训练闪彩个数
 
 
   //训练数值计算的中间变量，存下来方便手写逻辑进行估计
@@ -160,7 +276,7 @@ struct Game
 public:
 
   void newGame(std::mt19937_64& rand,
-    bool enablePlayerPrint,
+    GameSettings settings,
     int newUmaId,
     int umaStars,
     int newCards[6],
@@ -169,16 +285,14 @@ public:
 
 
   //这个操作是否允许且合理
-  //不允许的包括：菜数不够、已经做菜再次做菜，ura三个比赛回合选择训练不为空
-  //不合理的包括：（无法排除做某个菜点另一个训练，还在想能不能排除一部分）
   bool isLegal(Action action) const;
 
-  //进行Action后一直往后进行，直到下一次需要玩家决策（跳过比赛回合）。如果回合数>=78则什么都不做直接return（但不要报错或者闪退）
-  //注：若Action不包含训练，则吃菜但不进行训练，也不进入下一回合
-  //URA期间，比赛回合也让玩家进行吃菜决策，吃完菜进入下一回合
+  //无论什么stage，都往下进行一步，若action不合法则返回false
   void applyAction(
     std::mt19937_64& rand,
     Action action);
+
+
 
   int finalScore() const;//最终总分
   int finalScore_rank() const;//评价点
@@ -188,14 +302,19 @@ public:
 
 
 
+
   //原则上这几个private就行，如果private在某些地方非常不方便那就改成public
 
-  void autoUpgradeFarm(bool beforeXiahesu);//农田升级策略用手写逻辑处理，就不额外计算了，beforeXiahesu是夏合宿前那个回合收菜后进行升级
-  void randomDistributeCards(std::mt19937_64& rand);//随机分配人头
+  void randomDistributeCards(std::mt19937_64& rand);//随机分配人头，ST_distribute->ST_train
   void calculateTrainingValue();//计算所有训练分别加多少，并计算失败率、训练等级提升等
-  bool makeDish(int16_t dishId, std::mt19937_64& rand);//做菜，并处理相关收益，计算相关数值
-  bool applyTraining(std::mt19937_64& rand, int train);//处理 训练/出行/比赛 本身，包括友人点击事件，不包括做菜，不包括固定事件和剧本事件。如果不合法，则返回false，且保证不做任何修改
-  void checkEventAfterTrain(std::mt19937_64& rand);//检查固定事件和随机事件，并进入下一个回合
+  bool applyTraining(std::mt19937_64& rand, int16_t train);//ST_train->ST_decideEvent/ST_pickBuff/ST_event。处理 训练/出行/比赛 本身，不包括友人点击事件，不包括买buff，不包括固定事件和剧本事件。如果不合法，则返回false，且保证不做任何修改
+  void decideEvent(int16_t idx);//团卡三选一/出行选择
+  void decideEvent_outing(int16_t idx);//团卡出行选择
+  void decideEvent_three(int16_t idx);//团卡三选一
+  void randomPickBuff(std::mt19937_64& rand);//ST_pickBuff->ST_chooseBuff，从buff(心得)池里随机抽取buff
+  void chooseBuff(int16_t idx); //ST_chooseBuff->ST_event，选择第几个buff
+  
+  void checkEvent(std::mt19937_64& rand);//ST_chooseBuff->ST_distribute检查固定事件和随机事件，并进入下一个回合
 
   void checkFixedEvents(std::mt19937_64& rand);//每回合的固定事件，包括剧本事件和固定比赛和部分马娘事件等
   void checkRandomEvents(std::mt19937_64& rand);//模拟支援卡事件和随机马娘事件（随机加羁绊，体力，心情，掉心情等）
@@ -229,16 +348,14 @@ public:
   void addAllStatus(int value);//同时增加五个属性值
   void addVital(int value);//增加或减少体力，并处理溢出
   void addVitalMax(int value);//增加体力上限，限制120
-  void addMotivation(int value);//增加或减少心情，同时考虑“isPositiveThinking”
-  void addJiBan(int idx,int value,bool ignoreAijiao);//增加羁绊，并考虑爱娇。料理的羁绊不会变所以ignoreAijiao=true
+  void addMotivation(int value);//增加或减少心情，同时考虑“isPositiveThinking和蓝登
+  void addJiBan(int idx,int value,bool ignoreAijiao);//增加羁绊，并考虑爱娇和buff，也考虑红登充电。
   void addYayoiJiBan(int value);//增加理事长羁绊，剧本比赛等情况
   int getYayoiJiBan() const;//获得理事长羁绊
   void addStatusFriend(int idx, int value);//友人卡事件，增加属性值或者pt（idx=5），考虑事件加成
   void addVitalFriend(int value);//友人卡事件，增加体力，考虑回复量加成
   void runRace(int basicFiveStatusBonus, int basicPtBonus);//把比赛奖励加到属性和pt上，输入是不计赛后加成的基础值
   void addTrainingLevelCount(int trainIdx, int n);//为某个训练增加n次计数
-  void checkDishPtUpgrade();//在回合后，检查料理pt是否跨段，如果跨段则更新得意率或提升训练等级
-  void updateDeyilv();//在dishPt升级后，更新得意率
 
   int getTrainingLevel(int trainIdx) const;//计算训练等级
   int calculateFailureRate(int trainType, double failRateMultiply) const;//计算训练失败率，failRateMultiply是训练失败率乘数=(1-支援卡1的失败率下降)*(1-支援卡2的失败率下降)*...
@@ -247,29 +364,11 @@ public:
   //bool trainShiningCount(int trainIdx) const;    // 指定训练彩圈数 //uaf不一定有用
   void calculateTrainingValueSingle(int tra);//计算每个训练加多少   //uaf剧本可能五个训练一起算比较方便
 
-  //做菜相关
-  int maxFarmPtUntilNow() const;//假如全程绿圈，获得的农田pt数减去已经升级的pt数
-  bool upgradeFarm(int item);//把第item个农田升1级，失败返回false
-  void addDishMaterial(int idx, int num);//增加菜材料，并处理溢出
-  bool isDishLegal(int dishId) const;//此料理是否允许
-  int getDishTrainingBonus(int trainIdx) const;//计算当前料理的训练加成
-  int getDishRaceBonus() const;//计算当前料理的比赛加成
-  void handleDishBigSuccess(int dishId, std::mt19937_64& rand);//处理料理大成功相关的事件
-  std::vector<int> dishBigSuccess_getBuffs(int dishId, std::mt19937_64& rand);//料理大成功-获取有哪些buff
-  void dishBigSuccess_hint(std::mt19937_64& rand);//料理大成功-技能hint
-  void dishBigSuccess_invitePeople(int trainIdx, std::mt19937_64& rand);//料理大成功的分身效果：往trainIdx随机分配一个支援卡
-  int turnIdxInHarvestLoop() const;//收获周期里的第几回合(turn%4)。夏合宿恒为0
-  void addFarm(int type, int extra, bool isGreen);//种田，extra是人头附加，isGreen是绿圈
-  std::vector<int> calculateHarvestNum(bool isAfterTrain) const;//收获五种菜的数量，以及农田pt数
-  void maybeHarvest();//每4回合收菜，合宿每回合收菜，不是收菜回合就直接return
-  void maybeCookingMeeting();//试食会
-
-
 
   //友人卡相关事件
   void handleFriendUnlock(std::mt19937_64& rand);//友人外出解锁
   void handleFriendOutgoing(std::mt19937_64& rand);//友人外出
-  void handleFriendClickEvent(std::mt19937_64& rand, int atTrain);//友人点击事件（お疲れ様）
+  void handleFriendClickEvent(std::mt19937_64& rand, int atTrain);//友人点击事件
   void handleFriendFixedEvent();//友人固定事件，拜年+结算
   
 
